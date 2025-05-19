4c3bafe3d550d9d81d7396bd0a379a5aab800d01
Oskar Wiktor <90034719+OskarWiktor@users.noreply.github.com>
2025-05-19 20:53:08 +0200

    feat: project init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function App(props) {
+  
+  return (
+    <main>
+      <div className='pattern' />
+
+      <div className='wrapper'>
+        <header>
+          <h1>Find <span className='text-gradient'>Movie</span>You'll Enjoy Without the Hassle</h1>
+        </header>
+      </div>
+    </main>
+  )
+}
